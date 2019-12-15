3e905b440efc0f32653d32ddc2425b9c66f7b565
Vita <vitakozlovska5@gmail.com>
2019-12-15 19:56:43 +0200

    final change

--- source/js/script.js
+++ source/js/script.js
@@ -8,13 +8,13 @@ var lng;
 
 function initMap() {
   if (window.matchMedia("(min-width: 1300px)").matches) {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById("map"), {
       center: {lat:59.9387942, lng:30.3200800},
       zoom: 17
       });
     }
     else {
-      map = new google.maps.Map(document.getElementById('map'), {
+      map = new google.maps.Map(document.getElementById("map"), {
       center: {lat:59.9387942, lng:30.3230833},
       zoom: 17
       });
@@ -30,18 +30,18 @@ function initMap() {
 
 // Навигация
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
 
@@ -58,7 +58,7 @@ buttonBefore.addEventListener("click", function() {
 }
 });
 
-buttonAfter.addEventListener('click', function () {
+buttonAfter.addEventListener("click", function () {
 if (!progress.classList.contains("example__image-wrapper--show")) {
   progress.classList.add("example__image-wrapper--show");
 }
